a36ee05501f4e520910667821f1d862f4720e12e
Andres Sainz <andresmsainz@gmail.com>
2021-04-27 14:50:30 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fundamentals_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fundamentals Part 3!");
+            Console.WriteLine("Try Parse... Could be an int or it could not!");
+            Console.WriteLine("Please input an int higher than 0");
+            int.TryParse(Console.ReadLine(), out int x);
+
+            if (x==0)
+            {
+                Console.WriteLine("This is not valid");
+            }
+            else
+            {
+                Console.WriteLine("You have entered " + x);
+            }
+        }
+    }
+}
