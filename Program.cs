9f5976cb50ab223c7811afa8ea660989fb2c54ba
Andres Sainz <andresmsainz@gmail.com>
2021-04-27 15:15:15 -0400

    Added constants section

--- Program.cs
+++ Program.cs
@@ -8,18 +8,53 @@ namespace Fundamentals_3
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Fundamentals Part 3!");
-            Console.WriteLine("Try Parse... Could be an int or it could not!");
-            Console.WriteLine("Please input an int higher than 0");
-            int.TryParse(Console.ReadLine(), out int x);
+            bool bTryParse = false;
+            bool bConstants = true;
 
-            if (x==0)
+            //Try Parse
+            if (bTryParse == true)
             {
-                Console.WriteLine("This is not valid");
+                Console.WriteLine("Try Parse... Could be an int or it could not!");
+                Console.WriteLine("Please input an int higher than 0");
+                int.TryParse(Console.ReadLine(), out int x);
+
+                if (x == 0)
+                {
+                    Console.WriteLine("This is not valid");
+                }
+                else
+                {
+                    Console.WriteLine("You have entered " + x);
+                }
             }
             else
             {
-                Console.WriteLine("You have entered " + x);
+                Console.WriteLine("Skipping TryParse per Configuration");
             }
+
+            //Constants Section
+            if (bConstants == true)
+            {
+                Console.WriteLine("Constants... Values are not allowed to change!");
+
+                const double dblGravityForce = 9.8;
+                const double dblPI = 3.14159265359;
+
+                Console.WriteLine("Gravity Force is " + dblGravityForce + " & PI is " + dblPI);
+            }
+            else
+            {
+                Console.WriteLine("Skipping Constants per Configuration");
+            }
+
+            //Next Section
+            //Next Section
+            //Next Section
+            //Next Section
+            //Next Section
+            //Next Section
+
+
         }
     }
 }
